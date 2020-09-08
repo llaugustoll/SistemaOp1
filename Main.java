be71d09cd7b0353d2214799f0afceda3fa7488c4
llaugustoll <53905902+llaugustoll@users.noreply.github.com>
2020-09-07 23:06:53 -0300

    Update Main.java
    Atualizado com finalização do processo

--- Main.java
+++ Main.java
@@ -6,14 +6,18 @@ public class Main {
 	public static void main(String[] args) {
 		Processos opSistema = new Processos();
 		String os = opSistema.Sistema();
-		System.out.println(os);
+	
 		String a = "Windows";
 		String ar[] = new String[3];
 		ar = os.split(" ");
 		String b = ar[0];
-		System.out.println(ar[0]);
+		
+
 		String process = "TASKLIST /FO TABLE";
+		//String param = "10152";	
 		opSistema.Processos(a, b , process);
+		//String param = "cmd.exe";
+		//opSistema.killProcess(param);
 		
 	}
 }
