6212c7583d6cc7155949359343d65f323c7a1828
llaugustoll <53905902+llaugustoll@users.noreply.github.com>
2020-09-08 14:28:16 -0300

    Processos Finalizado

--- Processos.java
+++ Processos.java
@@ -16,7 +16,7 @@ public class Processos {
 		return os;
 	}
 	
-	public void Processos(String a , String b , String process) {
+	public void Processos(String a , String b , String l ,String process, String processl) {
 		if(b.intern() == a) {
 			try {
 				Process p = Runtime.getRuntime().exec(process);
@@ -36,35 +36,79 @@ public class Processos {
 			}
 			
 		}
-		else {
-			System.out.println("os" +"\n" + a +" "+ b);
+		if (b.intern() == l){
+			
+			try {
+				Process p = Runtime.getRuntime().exec(processl);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer  = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					System.out.println(linha);
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		}
 		
 	}
 
-	public void killProcess(String param) {
-		String cmdPid = "TASKKILL /PID";
-		String cmdNome = "TASKKILL /IM";
-		int pid = 0 ;
-		StringBuffer buffer = new StringBuffer();
-		
-		try {
-			pid = Integer.parseInt(param);
-			buffer.append(cmdPid);
-			buffer.append(" ");
-			buffer.append(pid);		
-		}catch (NumberFormatException e) {
-			buffer.append(cmdNome);
-			buffer.append(" ");
-			buffer.append(param);
+	public void killProcess(String param ,String a , String b , String c) {
+		if(c.intern() == a ) { 
+			String cmdPid = "TASKKILL /PID";
+			String cmdNome = "TASKKILL /IM";
+			int pid = 0 ;
+			StringBuffer buffer = new StringBuffer();
+			
+			try {
+				pid = Integer.parseInt(param);
+				buffer.append(cmdPid);
+				buffer.append(" ");
+				buffer.append(pid);		
+			}catch (NumberFormatException e) {
+				buffer.append(cmdNome);
+				buffer.append(" ");
+				buffer.append(param);
+			}
+			
+			try {
+				Runtime.getRuntime().exec(buffer.toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-		try {
-			Runtime.getRuntime().exec(buffer.toString());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (c.intern() == b) {
+				String cmdPid = "kill -9";
+				String cmdNome = "pkill -9";
+				int pid = 0 ;
+				StringBuffer buffer = new StringBuffer();
+				
+				try {
+					pid = Integer.parseInt(param);
+					buffer.append(cmdPid);
+					buffer.append(" ");
+					buffer.append(pid);		
+				}catch (NumberFormatException e) {
+					System.out.println("aqui "+param);
+					buffer.append(cmdNome);
+					buffer.append(" ");
+					buffer.append(param);
+				}
+				
+				try {
+					Runtime.getRuntime().exec(buffer.toString());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
 		}
-		
 	}
-}
